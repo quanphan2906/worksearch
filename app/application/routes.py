7b836455c78daf3ccca3f5f5b64a41681199ed5e
Quan Phan <hoangquanphan2906@gmail.com>
2023-10-21 20:14:56 +0300

    Function to create an application

--- app/application/routes.py
+++ app/application/routes.py
@@ -1,7 +1,39 @@
 from flask import Blueprint, request, jsonify
 
 from ..extension import db
-from ..models import User, Application
+from ..models import Application
 
 
 application = Blueprint("application", __name__, url_prefix="/application")
+
+
+# add an application for existing job posting
+@application.route("/", methods=["POST"])
+def apply():
+    data = request.json
+    user_email = data.get("user_email")
+    job_id = data.get("job_id")
+    application_date = data.get("application_date")
+    cover_letter = data.get("cover_letter")
+
+    if not user_email:
+        return jsonify({"message": "Missing email", "status": 400})
+    if not job_id:
+        return jsonify({"message": "Missing job id", "status": 400})
+
+    new_application = Application(
+        user_email=user_email,
+        job_id=job_id,
+        application_date=application_date,
+        cover_letter=cover_letter,
+    )
+
+    try:
+        db.session.add(new_application)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(
+            {"message": str(e.args[0]), "status": getattr(e, "status", None)}
+        )
+
+    return jsonify({"message": "Successfully applied", "status": 200})
